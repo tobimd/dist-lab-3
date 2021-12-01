a1eee23edc8c12afb0beae4c84b7e103e69f93ab
ecarrill <esteban.carrillo@sansano.usm.cl>
2021-12-01 11:05:12 -0300

    add check for missing output log dir

--- common/log/log.go
+++ common/log/log.go
@@ -1,6 +1,7 @@
 package log
 
 import (
+	"errors"
 	"fmt"
 	"log"
 	"os"
@@ -10,7 +11,8 @@ import (
 const (
 	LstdAppendFlags       = os.O_APPEND | os.O_CREATE | os.O_WRONLY
 	LstdLogFlags          = log.LstdFlags | log.Lmicroseconds
-	formattedLogOutputDir = "output_logs/%s"
+	outputDir             = ".logs"
+	formattedLogOutputDir = outputDir + "/%s"
 )
 
 var (
@@ -19,16 +21,23 @@ var (
 
 // Print formatted message to stdout as well as to log file
 func Print(f *string, msg string, a ...interface{}) {
-	fmt.Printf(fmt.Sprintf(msg, a...))
+	fmt.Printf(fmt.Sprintf(msg, a...) + "\n")
 	Log(f, msg, a...)
 }
 
 // Append formatted message to log file
 func Log(f *string, msg string, a ...interface{}) {
-	if f == nil {
+	// Format file name to have prefix
+	if f == nil || *f == "" {
 		f = &fallbackFileName
 	}
 	prefixedFilename := fmt.Sprintf(formattedLogOutputDir, *f)
+
+	// Check if output log dir exists, if not, then create
+	if !FileExists(outputDir) {
+		err := os.Mkdir(outputDir, 0755)
+		FailOnError(f, err, "Couldn't create directory \"%s\" for logs", outputDir)
+	}
 
 	file, err := os.OpenFile(prefixedFilename, LstdAppendFlags, 0600)
 	FailOnError(&fallbackFileName, err, "Couldn't open file \"%s\"", prefixedFilename)
@@ -51,4 +60,10 @@ func FailOnError(f *string, err error, msg string, a ...interface{}) {
 func Fatal(f *string, msg string, a ...interface{}) {
 	Print(f, "[ FATAL ! ] %s\n", fmt.Sprintf(msg, a...))
 	syscall.Exit(1)
+}
+
+// Return true if file exists, false otherwise
+func FileExists(filename string) bool {
+	_, err := os.Stat(filename)
+	return !errors.Is(err, os.ErrNotExist)
 }
