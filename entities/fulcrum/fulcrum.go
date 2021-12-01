c40ae69c14cc7793d56037db6764e7d3d39c9f05
ecarrill <esteban.carrillo@sansano.usm.cl>
2021-11-30 23:37:45 -0300

    wip: update SavePlanetData
    still needs to include UpdatePlanetLog into the workflow

--- entities/fulcrum/fulcrum.go
+++ entities/fulcrum/fulcrum.go
@@ -10,40 +10,56 @@ import (
 var (
 	id int
 	f  = ""
+
+	// Contains a growing map where the keys are the names of
+	// each planet registered, and the values are the
+	// corresponding time vector for each one
+	planetTimeVectors = make(map[string]TimeVector)
 )
+
+type TimeVector struct {
+	times []int
+}
 
 // Open (or create) a registry file corresponding to a specific
 // planet and add the corresponding information onto it.
 //
 // For planet "Tatooine", city "Mos Eisley" and number of rebels
 // "5", all info is stored in the following way:
-//
-//     -- Tatooine.txt
-//     Tatooine Mos_Eisley 5
-//
-func SavePlanetData(planet string, city string, numRebels int) {
+//     -- Tatooine.txt: Tatooine Mos_Eisley 5
+func SavePlanetData(planet string, city string, numRebels int) TimeVector {
+	// Set the filename and the data to save
 	filename := planet + ".txt"
-	data := fmt.Sprintf("%s %s %d", planet, city, numRebels)
+	info := fmt.Sprintf("%s %s %d", planet, city, numRebels)
 
-	err := util.WriteLines(filename, data)
+	// Write data into file
+	fileExisted, err := util.WriteLines(filename, info)
 	log.FailOnError(&f, err, "Couldn't write to file \"%s\"", filename)
-}
 
-func LoadPlanetData(planet string) {
+	// Create time vector for new planet, because there was no
+	// file for it before
+	if !fileExisted {
+		planetTimeVectors[planet] = TimeVector{times: make([]int, 3)}
+	}
 
+	// Update time vector for that planet
+	planetTimeVectors[planet].times[id] += 1
+
+	// Return the corresponding time vector
+	return planetTimeVectors[planet]
 }
 
 func UpdatePlanetLog(command data.CommandEnum, planet string, city string, value interface{}) {
 	filename := fmt.Sprintf("log.%s.txt", planet)
-	data := ""
+	info := ""
 
-	if value != nil {
-		data = fmt.Sprintf("%s %s %s %v", command.Name, planet, city, value)
+	if command.Enum != data.Command.DELETE_CITY.Enum {
+		info = fmt.Sprintf("%s %s %s %v", command.Name, planet, city, value)
 	} else {
-		data = fmt.Sprintf("%s %s %s", command.Name, planet, city)
+		info = fmt.Sprintf("%s %s %s", command.Name, planet, city)
 	}
 
-	err := util.WriteLines(filename, data)
+	_, err := util.WriteLines(filename, info)
 	log.FailOnError(&f, err, "Couldn't write to file \"%s\"", filename)
 }
 
