3afd6256c1c4dc3f1003e1f367eccae1e172e0de
tobimd <tobimd@users.noreply.github.com>
2021-12-11 17:56:57 -0300

    finish sync/consistency related functions

--- entities/fulcrum/fulcrum.go
+++ entities/fulcrum/fulcrum.go
@@ -24,7 +24,23 @@ var (
 	// to pb.CommuncationClient which should be what gets
 	// returned by `util.SetupClient(...)`
 	clients = make(map[string]*Client)
+
+	// When using 'SavePlanetData', determine wether to append
+	// to the planet's history file, or rewrite it completely
+	// which is used when merging.
+	StoreMethod = struct{ Append, Rewrite Method }{Append: 0, Rewrite: 1}
+
+	// When leader fulcrum (id=0) tells neighbors to send their
+	// history to it, the server function BroadcastChanges will
+	// be called on leader and the response should be the merge
+	// from all three histories. To solve that issue, this
+	// channel will recieve boolean true when both histories
+	// have been stored
+	neighborChangesCh = make(chan bool)
+	neighborHistories = struct{ hist1, hist2 []*pb.CommandParams }{}
 )
+
+type Method uint8
 
 // Open (or create) a registry file corresponding to a specific
 // planet and add the corresponding information onto it.
@@ -32,10 +48,14 @@ var (
 // For planet "Tatooine", city "Mos Eisley" and number of rebels
 // "5", all info is stored in the following way:
 //     -- Tatooine.txt: Tatooine Mos_Eisley 5
-func SavePlanetData(planet string, city string, numRebels int) data.TimeVector {
+func SavePlanetData(planet string, city string, numRebels int, storeMethod Method) data.TimeVector {
 	// Set the filename and the data to save
 	filename := planet + ".txt"
 	info := fmt.Sprintf("%s %s %d", planet, city, numRebels)
+
+	if storeMethod == StoreMethod.Rewrite {
+		util.DeleteFile(filename)
+	}
 
 	// Write data into file
 	fileExisted, err := util.WriteLines(filename, info)
@@ -70,16 +90,33 @@ func UpdatePlanetLog(command *pb.Command, planet string, city string, value inte
 	log.FailOnError(&f, err, "Couldn't write to file \"%s\"", filename)
 }
 
-func ReadPlanetLog(planet string) []pb.CommandParams {
+func ReadPlanetLog(planet string) []*pb.CommandParams {
 	filename := fmt.Sprintf("log.%s.txt", planet)
-	info := make([]pb.CommandParams, 1)
+	info := make([]*pb.CommandParams, 1)
 
 	util.ReadLines(filename,
 		func(line string) bool {
 			// <command> <planet> <city> [<new_value>]
 			values := strings.Split(line, " ")
-			info = append(info, pb.CommandParams{
-				Command: pb.CommandFromString(values[0]),
+
+			command := pb.CommandFromString(values[0])
+			newName := ""
+			var newNumberOfRebels uint32 = 0
+
+			if len(values) == 4 {
+				if *command == pb.Command_UPDATE_NAME {
+					newName = values[3]
+				} else {
+					newNumberOfRebels = util.StringToUint32(values[3])
+				}
+			}
+
+			info = append(info, &pb.CommandParams{
+				Command:        command,
+				PlanetName:     &planet,
+				CityName:       &values[2],
+				NewCityName:    &newName,
+				NewNumOfRebels: &newNumberOfRebels,
 			})
 
 			return false
@@ -87,6 +124,15 @@ func ReadPlanetLog(planet string) []pb.CommandParams {
 	)
 
 	return info
+}
+
+func MergeHistories() []*pb.CommandParams {
+
+	// TODO: Implement merging strategy (using neighborHistories)
+
+	var newHistory []*pb.CommandParams
+
+	return newHistory
 }
 
 // Asynchronous function called only by one of the available
@@ -106,9 +152,6 @@ func SyncWithEventualConsistency() {
 				Command: pb.Command_CHECK_CONSISTENCY.Enum(),
 			})
 		}
-
-		// Await for them to  send 'BroadcastChanges' rpc call
-		// and use that to respond with the new merged history
 	}
 }
 
--- entities/fulcrum/rpc.go
+++ entities/fulcrum/rpc.go
@@ -29,15 +29,62 @@ func (s *Server) RunCommand(ctx context.Context, command *pb.CommandParams) (*pb
 	case pb.Command_CHECK_CONSISTENCY.Enum():
 		// When recieving this command, gather all history from
 		// logs and call `BroadcastChanges` to sender with defer
+		info := make([]*pb.CommandParams, 0)
 
-		for planet, _ /*vector*/ := range planetVectors {
-			/*info :=*/ ReadPlanetLog(planet)
+		for planet, vector := range planetVectors {
+			// Append all updates done to a planet
+			info = append(info, ReadPlanetLog(planet)...)
 
+			// Get the three times for each Fulcrum
+			fTime0 := uint32(vector.Time[0])
+			fTime1 := uint32(vector.Time[1])
+			fTime2 := uint32(vector.Time[2])
+
+			// For the last Command, set LastTimeVector with these
+			info[len(info)-1].LastTimeVector = &pb.TimeVector{
+				Time: []uint32{fTime0, fTime1, fTime2},
+			}
 		}
+
+		// Now that all history is saved within 'info', call
+		// 'BroadcastChanges' to fulcrum 0
+		defer clients[data.Address.FULCRUM[0]].BroadcastChanges(&pb.FulcrumHistory{
+			History: info,
+		})
 
 	}
 
 	return &pb.Empty{}, nil
+}
+
+// Should only be called to server Fulcrum with id 0, because
+// it's the only one assign to merge all three action histories.
+//
+// The lower the id, the bigger priority it has over merge
+// conflicts, so that way we can avoid having to recieve both of
+// the neighbor's histories and just deal with them separately.
+func (s *Server) BroadcastChanges(ctx context.Context, history *pb.FulcrumHistory) (*pb.FulcrumHistory, error) {
+	log.Log(&f, "[server:BroadcastChanges] Called")
+
+	// TODO: Check if it makes sense to use this logic inside
+	// an anonymous function (and also that it's async)
+	go func() {
+		if len(neighborHistories.hist1) == 0 {
+			neighborHistories.hist1 = history.GetHistory()
+
+		} else {
+			neighborHistories.hist2 = history.GetHistory()
+			neighborChangesCh <- true
+		}
+	}()
+
+	// Stop until second fulcrum sends history, making the
+	// channel 'neighborChangesCh' recieve a value
+	<-neighborChangesCh
+
+	newHistory := MergeHistories()
+
+	return &pb.FulcrumHistory{History: newHistory}, nil
 }
 
 // **** CLIENT FUNCTIONS ****
@@ -51,7 +98,39 @@ func (c *Client) RunCommand(command *pb.CommandParams) *pb.Empty {
 	defer cancel()
 
 	res, err := client.RunCommand(ctx, command)
-	log.FailOnError(&f, err, "Couldn't call \"RunCommand\" as client")
+	log.FailOnError(&f, err, "Couldn't call \"RunCommand\" as entity Fulcrum (id=%d)", id)
 
 	return res
+}
+
+func (c *Client) BroadcastChanges(history *pb.FulcrumHistory) {
+	client := *((*c).Client)
+	log.Log(&f, "[client:BroadcastChanges] Called")
+
+	ctx, cancel := util.GetContext()
+	defer cancel()
+
+	res, err := client.BroadcastChanges(ctx, history)
+	log.FailOnError(&f, err, "Couldn't call \"BroadcastChanges\" as entity Fulcrum (id=%d)", id)
+
+	// The returned value should be considered as the new
+	// history for each planet. Because fulcrum with id=0 did
+	// the merging strategy on all three histories and returned
+	// the new values
+
+	currPlanet := ""
+
+	for _, history := range res.GetHistory() {
+		planet := history.GetPlanetName()
+		city := history.GetCityName()
+		numRebels := int(history.GetNumOfRebels())
+
+		method := StoreMethod.Append
+		if planet != currPlanet {
+			method = StoreMethod.Rewrite
+			currPlanet = planet
+		}
+
+		SavePlanetData(planet, city, numRebels, method)
+	}
 }
