07037fca7203564b58762a8d31c08c765276b570
BooterMan98 <cesarpaulangelo@gmail.com>
2021-12-13 00:04:47 -0300

    temp fix: default response values for leia

--- entities/leia/leia.go
+++ entities/leia/leia.go
@@ -10,7 +10,7 @@ import (
 var (
 	f = "leia.log"
 
-	cmdHistory = make([]data.CommandHistory, 1)
+	cmdHistory = make([]data.CommandHistory, 0)
 )
 
 func Run() {
@@ -32,20 +32,31 @@ func Run() {
 
 		serverResponse := leia_client.RunCommand(&command)
 
-		numRebels := serverResponse.NumOfRebels
-		timeVector := serverResponse.TimeVector.Time
-		fulcrumAddr := serverResponse.FulcrumRedirectAddr
+		log.Log(&f, "Broker Response: %v", *serverResponse)
+
+		///////////////////////  Uncomment when broker responds with something... //////////////////////////
+		// numRebels := *serverResponse.NumOfRebels
+		// timeVector := serverResponse.TimeVector.Time
+		// fulcrumAddr := *serverResponse.FulcrumRedirectAddr
+
+		numRebels := 20
+		timeVector := data.TimeVector{1, 1, 1}
+		fulcrumAddr := data.Address.FULCRUM[2]
+
+		log.Log(&f, "%v, %v, %v", numRebels, timeVector, fulcrumAddr)
 
 		event := data.CommandHistory{
 			Command:        *cmd,
 			Planet:         planet,
-			FulcrumAddress: *fulcrumAddr,
+			FulcrumAddress: fulcrumAddr,
 			TimeVector:     timeVector,
 		}
 
 		// save event in command history
 		cmdHistory = append(cmdHistory, event)
 
-		log.Print(&f, "Number of Rebels: %d\n", *numRebels)
+		log.Print(&f, "Number of Rebels: %d\n", numRebels)
+
+		log.Log(&f, "History to date: %+v", cmdHistory)
 	}
 }
